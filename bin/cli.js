796bc0567892802845fec6ca88ec99f8223c9964
yunjaekim <yunjaekim007@gmail.com>
2024-03-07 10:58:36 +0900

    testing for rest api

---
+++ bin/cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const { parseArgs } = require('node:util');
+const next = require('next');
+
+const args = parseArgs({
+	options: {
+		port: {
+			type: 'number',
+			short: 'p',
+		},
+	},
+});
+
+const port = args.values.port || 8888;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+	require('http').createServer((req, res) => {
+		handle(req, res);
+	}).listen(port, (err) => {
+		if (err) throw err;
+		console.log(`> Ready on http://localhost:${port}`);
+	});
+});
